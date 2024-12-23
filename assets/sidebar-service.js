d4f17de6794a1be47357d82320a2340a2897301d
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-12-23 21:22:39 +0500

    sedebar qoshildi

--- assets/sidebar-service.js
+++ assets/sidebar-service.js
@@ -1,14 +1,22 @@
 const mobileSelectedButton = document.querySelector(".sidebar-selected");
 
 function toggleSidebar() {
-    mobileSelectedButton.classList.toggle("sidebar-selected--open");
-    document.querySelector(".sidebar").classList.toggle("screen");
+  mobileSelectedButton.classList.toggle("sidebar-selected--open");
+  document.querySelector(".sidebar").classList.toggle("screen");
 
-    if (document.body.style.overflow === "hidden") {
-        document.body.style.overflow = "";
-    } else {
-        document.body.style.overflow = "hidden";
-    }
+  if (document.body.style.overflow === "hidden") {
+    document.body.style.overflow = "";
+  } else {
+    document.body.style.overflow = "hidden";
+  }
 }
 
+const accordionButtons = document.querySelectorAll(
+  ".sidebar__menu-item--accordion"
+);
 
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("sidebar__menu-item--open");
+  });
+});

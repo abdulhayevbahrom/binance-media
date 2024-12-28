0d54e3f42ea868b79176197799a3b59d227af47e
azimjon-95 <>
2024-12-28 11:06:30 +0500

    my 4454

--- assets/sidebar-service.js
+++ assets/sidebar-service.js
@@ -1,7 +1,7 @@
-const mobileSelectedButton1 = document.querySelector(".sidebar-selected");
+const mobileSelectedButton = document.querySelector(".sidebar-selected");
 
 function toggleSidebar() {
-  mobileSelectedButton1.classList.toggle("sidebar-selected--open");
+  mobileSelectedButton.classList.toggle("sidebar-selected--open");
   document.querySelector(".sidebar").classList.toggle("screen");
 
   if (document.body.style.overflow === "hidden") {
@@ -11,11 +11,11 @@ function toggleSidebar() {
   }
 }
 
-const accordionButtons1 = document.querySelectorAll(
+const accordionButtons = document.querySelectorAll(
   ".sidebar__menu-item--accordion"
 );
 
-accordionButtons1.forEach((button) => {
+accordionButtons.forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("sidebar__menu-item--open");
   });

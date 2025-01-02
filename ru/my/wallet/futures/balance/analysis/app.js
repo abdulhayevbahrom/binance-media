9beab6244e1a991f0fd7af0524ea16c9642d881a
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-01-02 09:11:17 +0500

    fixed errors

--- ru/my/wallet/futures/balance/analysis/app.js
+++ ru/my/wallet/futures/balance/analysis/app.js
@@ -54,16 +54,17 @@ const pnl_today = async () => {
   let [title, qiymatlar] = today.children;
   let [percent, usd] = qiymatlar.children;
 
-  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${data?.percentage || 0
-    } %`;
+  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${
+    data?.percentage || 0
+  } %`;
   usd.innerHTML = `${data?.pnl > 0 ? "+" : ""}${data?.pnl || 0} USD`;
 
   percent.style.color =
     data?.percentage < 0
       ? "#f64460"
       : data?.percentage > 0
-        ? "#2ebd85"
-        : "#fff";
+      ? "#2ebd85"
+      : "#fff";
   usd.style.color =
     data?.pnl < 0 ? "#f64460" : data?.pnl > 0 ? "#2ebd85" : "#fff";
 };
@@ -89,16 +90,17 @@ const pnl_seven = async () => {
   let [title, qiymatlar] = seven.children;
   let [percent, usd] = qiymatlar.children;
 
-  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${data?.percentage || 0
-    } %`;
+  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${
+    data?.percentage || 0
+  } %`;
   usd.innerHTML = `${data?.pnl > 0 ? "+" : ""}${data?.pnl || 0} USD`;
 
   percent.style.color =
     data?.percentage < 0
       ? "#f64460"
       : data?.percentage > 0
-        ? "#2ebd85"
-        : "#fff";
+      ? "#2ebd85"
+      : "#fff";
   usd.style.color =
     data?.pnl < 0 ? "#f64460" : data?.pnl > 0 ? "#2ebd85" : "#fff";
 };
@@ -123,16 +125,17 @@ const pnl_month = async () => {
   let [title, qiymatlar] = month.children;
   let [percent, usd] = qiymatlar.children;
 
-  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${data?.percentage || 0
-    } %`;
+  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${
+    data?.percentage || 0
+  } %`;
   usd.innerHTML = `${data?.pnl > 0 ? "+" : ""}${data?.pnl || 0} USD`;
 
   percent.style.color =
     data?.percentage < 0
       ? "#de5468"
       : data?.percentage > 0
-        ? "#2ebd85"
-        : "#fff";
+      ? "#2ebd85"
+      : "#fff";
   usd.style.color =
     data?.pnl < 0 ? "#de5468" : data?.pnl > 0 ? "#2ebd85" : "#fff";
 };
@@ -159,16 +162,17 @@ const pnl_all = async () => {
   let [title, qiymatlar] = all.children;
   let [percent, usd] = qiymatlar.children;
 
-  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${data?.percentage || 0
-    } %`;
+  percent.innerHTML = `${data?.percentage > 0 ? "+" : ""}${
+    data?.percentage || 0
+  } %`;
   usd.innerHTML = `${data?.pnl > 0 ? "+" : ""}${data?.pnl || 0} USD`;
 
   percent.style.color =
     data?.percentage < 0
       ? "#de5468"
       : data?.percentage > 0
-        ? "#2ebd85"
-        : "#fff";
+      ? "#2ebd85"
+      : "#fff";
   usd.style.color =
     data?.pnl < 0 ? "#de5468" : data?.pnl > 0 ? "#2ebd85" : "#fff";
 };
@@ -570,7 +574,7 @@ async function showData(start_date, end_date) {
               return value < 0 ? "#e74c3c" : "#11CB80"; // Qizil (salbiy), yashil (ijobiy)
             },
             borderWidth: 0,
-            hoverBackgroundColor: "#3498db", // Fon rangi kursor bilan
+            // hoverBackgroundColor: "#3498db", // Fon rangi kursor bilan
           },
         ],
       },
@@ -677,9 +681,11 @@ async function showData(start_date, end_date) {
 
       // Get today's date for comparison
       const today = new Date();
-      const todayDateString = `${today.getFullYear()}-${today.getMonth() + 1 < 10 ? "0" : ""
-        }${today.getMonth() + 1}-${today.getDate() < 10 ? "0" : ""
-        }${today.getDate()}`;
+      const todayDateString = `${today.getFullYear()}-${
+        today.getMonth() + 1 < 10 ? "0" : ""
+      }${today.getMonth() + 1}-${
+        today.getDate() < 10 ? "0" : ""
+      }${today.getDate()}`;
 
       // Create empty days before the first day
       for (let i = 0; i < firstDayIndex; i++) {
@@ -694,8 +700,9 @@ async function showData(start_date, end_date) {
         dayDiv.classList.add("day");
         dayDiv.innerText = day;
 
-        const currentDate = `${year}-${month + 1 < 10 ? "0" : ""}${month + 1}-${day < 10 ? "0" : ""
-          }${day}`;
+        const currentDate = `${year}-${month + 1 < 10 ? "0" : ""}${month + 1}-${
+          day < 10 ? "0" : ""
+        }${day}`;
 
         // Check if today's or a past date
         const isPastOrToday = new Date(currentDate) <= today;
@@ -745,30 +752,37 @@ async function showData(start_date, end_date) {
 
             modal.innerHTML = `
               <p style="margin-bottom:7px;">${currentDate}</p>
-              <p>Дневной PNL: <span style="color: ${datas?.dayli_pnl > 0
-                ? "#2ebd85"
-                : datas?.dayli_pnl < 0
+              <p>Дневной PNL: <span style="color: ${
+                datas?.dayli_pnl > 0
+                  ? "#2ebd85"
+                  : datas?.dayli_pnl < 0
                   ? "#e1545e"
                   : "#222"
-              };">${(datas?.dayli_pnl > 0 ? "+" : "") + datas?.dayli_pnl
-              } USD</span></p>
-              <p>Суммарный PNL: <span style="color: ${datas?.summ_pnl > 0
-                ? "#2ebd85"
-                : datas?.summ_pnl < 0
+              };">${
+              (datas?.dayli_pnl > 0 ? "+" : "") + datas?.dayli_pnl
+            } USD</span></p>
+              <p>Суммарный PNL: <span style="color: ${
+                datas?.summ_pnl > 0
+                  ? "#2ebd85"
+                  : datas?.summ_pnl < 0
                   ? "#e1545e"
                   : "#222"
-              };">${(datas?.summ_pnl > 0 ? "+" : "") + datas?.summ_pnl
-              } USD</span></p>
-              <p>Суммарный PNL %: <span style="color: ${datas?.summ_pnl_proc > 0
-                ? "#2ebd85"
-                : datas?.summ_pnl_proc < 0
+              };">${
+              (datas?.summ_pnl > 0 ? "+" : "") + datas?.summ_pnl
+            } USD</span></p>
+              <p>Суммарный PNL %: <span style="color: ${
+                datas?.summ_pnl_proc > 0
+                  ? "#2ebd85"
+                  : datas?.summ_pnl_proc < 0
                   ? "#e1545e"
                   : "#222"
-              };">${(datas?.summ_pnl_proc > 0 ? "+" : "") + datas?.summ_pnl_proc
-              } %</span></p>
-              <p>Суммарные переводы, нетто: <span style="color: ${datas?.summ_translations > 0
-                ? "#2ebd85"
-                : datas?.summ_translations < 0
+              };">${
+              (datas?.summ_pnl_proc > 0 ? "+" : "") + datas?.summ_pnl_proc
+            } %</span></p>
+              <p>Суммарные переводы, нетто: <span style="color: ${
+                datas?.summ_translations > 0
+                  ? "#2ebd85"
+                  : datas?.summ_translations < 0
                   ? "#e1545e"
                   : "#222"
               };">${datas?.summ_translations}</span></p>
@@ -1814,9 +1828,9 @@ async function showData_transaction(start_date, end_date) {
 }
 
 all_sum_komiccy_for_transaction_checkbox.onclick = (e) =>
-(all_sum_komiccy_for_transaction.style.display = e.target.checked
-  ? "block"
-  : "none");
+  (all_sum_komiccy_for_transaction.style.display = e.target.checked
+    ? "block"
+    : "none");
 
 // SHOW CHECKBOX TRANSAKTION
 let checkbox_transaktion_api = "/accounts/financing-and-transaction-fees";

39f8ac2b218c1bc0dd80da9646fcbbcfbb9e056c
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-01-24 19:11:00 +0500

    last commit

--- ru/my/orders/futures/service.js
+++ ru/my/orders/futures/service.js
@@ -1,62 +1,69 @@
 // Modal va tugmalarni olish
-const modal = document.querySelector('.order-filters_modal');
+const modal = document.querySelector(".order-filters_modal");
 const toggleButtons = [
-    document.querySelector('.order-filters-mobile > button'),
-    document.querySelector('.order-filters-mobile_fu > button'),
-    document.querySelector('.order-filters-mobile-tr > button')
+  document.querySelector(".order-filters-mobile > button"),
+  document.querySelector(".order-filters-mobile_fu > button"),
+  document.querySelector(".order-filters-mobile-tr > button"),
 ];
-const closeModalButton = document.querySelector('.close-modal');
+const closeModalButton = document.querySelector(".close-modal");
 
 // Modalni ochish funksiyasi
 toggleButtons.forEach((button) => {
-    if (button) {
-        button.addEventListener('click', () => {
-            modal.style.display = 'block';
-            modal.classList.add('active');
-            modal.classList.remove('closing');
-        });
-    }
+  if (button) {
+    button.addEventListener("click", () => {
+      modal.style.display = "block";
+      modal.classList.add("active");
+      modal.classList.remove("closing");
+      resetToDefault();
+      document.body.style.overflow = "hidden";
+    });
+  }
 });
 
 // Modalni yopish funksiyasi
 function closeModal() {
-    modal.classList.remove('active');
-    modal.classList.add('closing');
+  modal.classList.remove("active");
+  modal.classList.add("closing");
+  document.body.style.overflow = "auto";
 
-    // Animatsiya tugagandan so'ng modalni yashirish
-    modal.addEventListener('animationend', () => {
-        modal.classList.remove('closing');
-        modal.style.display = 'none';
-    }, { once: true });
+  // Animatsiya tugagandan so'ng modalni yashirish
+  modal.addEventListener(
+    "animationend",
+    () => {
+      modal.classList.remove("closing");
+      modal.style.display = "none";
+    },
+    { once: true }
+  );
 }
 
 // "Close" tugmachasini bosganda modalni yopish
 if (closeModalButton) {
-    closeModalButton.addEventListener('click', closeModal);
+  closeModalButton.addEventListener("click", closeModal);
 }
 
 // Modal tashqarisiga bosganda modalni yopish
-document.addEventListener('click', (event) => {
-    const isClickInsideModal = modal.contains(event.target);
-    const isClickOnToggleButton = toggleButtons.some(button => button && button.contains(event.target));
+document.addEventListener("click", (event) => {
+  const isClickInsideModal = modal.contains(event.target);
+  const isClickOnToggleButton = toggleButtons.some(
+    (button) => button && button.contains(event.target)
+  );
 
-    if (!isClickInsideModal && !isClickOnToggleButton) {
-        closeModal();
-    }
+  if (!isClickInsideModal && !isClickOnToggleButton) {
+    closeModal();
+  }
 });
 
 // ===================================
 // Select all the buttons
 function setActive(button) {
-    // Remove the 'selected' and 'active' classes from all buttons
-    const buttons = document.querySelectorAll('.order-select__label');
-    buttons.forEach(btn => {
-        btn.classList.remove('selected');
-        btn.classList.remove('active');
-    });
+  // Remove the 'selected' and 'active' classes from all buttons
+  const buttons = document.querySelectorAll(".order-select__label");
+  buttons.forEach((btn) => {
+    btn.classList.remove("selected");
+    btn.classList.remove("active");
+  });
 
-    // Add the 'selected' and 'active' classes to the clicked button
-    button.classList.add('selected', 'active');
+  // Add the 'selected' and 'active' classes to the clicked button
+  button.classList.add("selected", "active");
 }
-
-
